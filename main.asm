64d69b459596ada1a88a2f7a238bae485a8a5e5a
gbergs1994 <144708640+gbergs1994@users.noreply.github.com>
2024-06-02 17:20:35 +0000

    working on it

--- main.asm
+++ main.asm
@@ -5,27 +5,35 @@ reference, write a program that calculates the following expression,
 using registers: A = (A + B) - (C + D). Assign integer values to the EAX,
 EBX, ECX, and EDX registers. A=8000h, B= 2000h, C= 1000h, D=5000
 2. Using the Irvine library dump the registers to the screen to inspect your results
-3. Using the Irvine library write the followng string to the screen:
+3. Using the Irvine library write the following string to the screen:
 " My name is:  Your First and Last Name"
 !
 
-.386
-.model flat,stdcall
-.stack 4096
-ExitProcess proto,dwExitCode:dword
-.code
-main PROC
+ExitProcess proto
+; include irvine64.inc
+.data
+sum qword 0
+str1 byte ' My name is: Gunnar Bergstrom'
 
+.code
+main proc
 mov eax,8000h
 mov ebx,2000h
 mov ecx,1000h
 mov edx,5000h
 
-add eax, ebx; write yor comment here
-add ???, ???; write your comment here
+add eax, ebx; add the value of ebx to the eax register
+add ecx, edx; add the value of edx to the ecx register
 
-sub eax,ecx; write your comment here
+sub eax,ecx; subtract the value of ecx from the eax register
 
-INVOKE ExitProcess,0
-main ENDP
-END main
+; mov edx, offset str1
+
+lea edx,str1
+
+; call writestring
+
+call ExitProcess
+
+main endp
+end
